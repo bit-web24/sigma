1182b69789b2f3bf230a7c6e47d0d82ff23d2c55
bittu <bittu@pop-os.localdomain>
2021-05-31 11:49:56 +0530

    added license notice to files

--- src/ckeys.h
+++ src/ckeys.h
@@ -1,3 +1,19 @@
+/** This file is part of Sigma.
+
+   * Sigma is free software: you can redistribute it and/or modify
+   * it under the terms of the GNU General Public License as published by
+   * the Free Software Foundation, either version 3 of the License, or
+   * (at your option) any later version.
+
+   * Sigma is distributed in the hope that it will be useful,
+   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   * GNU General Public License for more details.
+
+   * You should have received a copy of the GNU General Public License
+   * along with Sigma.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef C_KEYS
 #define C_KEYS
 
--- src/main.c
+++ src/main.c
@@ -1,3 +1,23 @@
+/**
+ * Sigma - A Lightweight Text Editor
+ * /
+
+/** This file is part of Sigma.
+
+   * Sigma is free software: you can redistribute it and/or modify
+   * it under the terms of the GNU General Public License as published by
+   * the Free Software Foundation, either version 3 of the License, or
+   * (at your option) any later version.
+
+   * Sigma is distributed in the hope that it will be useful,
+   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   * GNU General Public License for more details.
+
+   * You should have received a copy of the GNU General Public License
+   * along with Sigma.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <ncurses.h>
 #include <stdlib.h>
